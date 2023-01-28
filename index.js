8762ed4a44ce36b481668c2394aacf84692e204d
Saksham Singh <developer.sakshamsingh@gmail.com>
2023-01-28 19:23:22 +0530

    Setup mock data

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ import managementRoutes from './routes/management.js';
 import salesRoutes from './routes/sales.js';
 
 /* CONFIGURATION */
-
 dotenv.config();
 const app = express();
 app.use(express.json());
@@ -27,3 +26,16 @@ app.use('/client', clientRoutes);
 app.use('/general', generalRoutes);
 app.use('/management', managementRoutes);
 app.use('/sales', salesRoutes);
+
+/* MONGOOSE SETUP */
+const PORT = process.env.PORT || 9000;
+mongoose.set('strictQuery', true);
+mongoose
+	.connect(process.env.MONGO_URL, {
+		useNewUrlParser    : true,
+		useUnifiedTopology : true
+	})
+	.then(() => {
+		app.listen(PORT, () => console.log(`Server Port : ${PORT}`));
+	})
+	.catch((error) => console.log(`ERROR : ${error}`));
