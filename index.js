29b090d0f7f767a9403a3fca07fcc9424c5da55e
Saksham Singh <developer.sakshamsingh@gmail.com>
2023-01-28 17:57:36 +0530

    "Setup folder structure"

--- index.js
+++ index.js
@@ -5,13 +5,18 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import helmet from 'helmet';
 import morgan from 'morgan';
+import clientRoutes from './routes/client.js';
+import generalRoutes from './routes/general.js';
+import managementRoutes from './routes/management.js';
+import salesRoutes from './routes/sales.js';
 
 /* CONFIGURATION */
+
 dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginOpenerPolicy({ policy: 'cross-origin' }));
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
 app.use(morgan('common'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
